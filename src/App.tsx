5380581f33d0a52fb2db355c98476753ae2dac84
Neelam Chavan <neelamchavan410@gmail.com>
2022-05-10 11:18:16 +0530

    get contacts based on tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import axios, { AxiosError } from 'axios'
 import Contacts from './components/Contacts';
 import Sidebar from './components/Sidebar';
 import Grid from '@mui/material/Grid';
@@ -6,29 +7,88 @@ import Box from '@mui/material/Box';
 
 const tagList = ["incididunt", "nulla", "reprehenderit", "ullamco", "velit", "enim", "magna", "quis", "sint", "duis", "occaecat", "dolore", "eu", "proident", "voluptate", "irure", "esse", "tempor", "ex" ]
 
-type checkedListType ={}
+type checkedListType = {}
+
+export type contactsType = contactType[]
+
+export type contactType = {
+  name: string
+  phoneNumber: string
+  id: string
+  tags: tagsType[]
+}
+
+type tagsType = {
+  name: string
+}
 
 function App() {
+  const [contacts, setContacts] = useState<contactsType[]>([])
   const [checkedList, setCheckedList] = useState<checkedListType[]>([])
-      const [checkedStateForInclude, setCheckedStateForInclude] = useState( new Array(tagList.length).fill(false));
+  const [checkedStateForInclude, setCheckedStateForInclude] = useState(new Array(tagList.length).fill(false));
+  let count = 0;
+  
+   useEffect(() => {
+    console.log(checkedList);
+    getContacts()
+    window.addEventListener('scroll', handleScroll)
+   }, [checkedList])
+  
+  const updateToken = async () => {
+    let res = await axios.post('https://api-teams.chatdaddy.tech/token',
+      {
+        "refreshToken": "059c420e-7424-431f-b23b-af0ecabfe7b8",
+        "teamId": "a001994b-918b-4939-8518-3377732e4e88"
+      }
+    )
+    if (res && res.data) {
+      console.log("new token",res);
+      localStorage.setItem('accessToken', res.data.access_token);
+    }
+  }
+
+  const getContacts = () => {
+    let token = localStorage.getItem('accessToken');
+    if (token) {      
+      axios.get(`https://api-im.chatdaddy.tech/contacts?count=${count}&tags=${checkedList}`,
+        {
+          headers: {
+            "Content-type": "Application/json",
+            "Authorization": `Bearer ${token}`
+          }
+        }
+      ).then(async response => {
+        console.log(response.data.contacts);
+        setContacts(response.data.contacts);
+      }).catch((error: AxiosError) => {
+        console.log(error.response);
+        updateToken().then(getContacts);
+      });
+    } else {
+      updateToken().then(getContacts);
+    }
+    count += 10
+  }
+
+  const handleScroll = (e:any) => {
+    if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight) {
+      getContacts()
+    }
+  }
+
 
   const handleIncludeToggle = (position: any) => {
-    //     const currentIndex:any = checkedList[position]
-    //     const newCheckedList = [...checkedList]
+       setCheckedStateForInclude((prevCheckedState:any) => {
+      const updatedCheckedState = prevCheckedState.map((item:boolean, index: any) => index === position ? !item : item);
+      console.log(updatedCheckedState);
 
-    //     if (currentIndex === -1) {
-    //         newCheckedList.push(tagList[position])
-    //     } else {
-    //         newCheckedList.splice(currentIndex, 1)
-    //     }
-    //   setCheckedList(newCheckedList)
-    // console.log(checkedList);
-    
-     const updatedCheckedState = checkedStateForInclude.map((item, index) =>
-      index === position ? !item : item
-    );
-    setCheckedStateForInclude(updatedCheckedState);
-    console.log(checkedStateForInclude);     
+      if (updatedCheckedState[position] === true) {
+        const newCheckedList = checkedList
+        newCheckedList.push(tagList[position])
+        console.log(checkedList)
+      }
+      return updatedCheckedState
+    });
   }
   
   return (
@@ -39,7 +99,7 @@ function App() {
           <Sidebar handleIncludeToggle={handleIncludeToggle} tagList={tagList} checkedStateForInclude={checkedStateForInclude}/>
           </Grid>
           <Grid item xs={6} sm={6} md={8} xl={9}>
-            <Contacts checkedList={checkedList} />
+            <Contacts checkedList={checkedList} contacts={contacts} />
           </Grid>
       </Grid>
     </Box>
--- src/components/Contacts.tsx
+++ src/components/Contacts.tsx
@@ -1,4 +1,3 @@
-import axios, { AxiosError } from 'axios'
 import React, { useState, useEffect } from 'react'
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
@@ -6,72 +5,28 @@ import Checkbox from '@mui/material/Checkbox';
 import Container from '@mui/material/Container';
 import ContactCard from './ContactCard'
 
-export type contactType = {
-  name: string
-  phoneNumber: string
-  id: string
-  tags: tagsType[]
-}
-type tagsType = {
-  name: string
-}
-
 type isCheckType = {}
 
 const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
+// interface Contact {
+//   name: string
+//   phoneNumber: string
+//   id: string
+//   tags: tagsType[]
+// }
+
+// type tagsType = {
+//   name: string
+// }
+
 function Contacts(props:any) {
-  let count = 0;
-  const [contacts, setContacts] = useState<contactType[]>([])
   const [isCheckAll, setIsCheckAll] = useState(false);
   const [isCheck, setIsCheck] = useState<isCheckType[]>([])
 
-  const updateToken = async () => {
-    let res = await axios.post('https://api-teams.chatdaddy.tech/token',
-      {
-        "refreshToken": "059c420e-7424-431f-b23b-af0ecabfe7b8",
-        "teamId": "a001994b-918b-4939-8518-3377732e4e88"
-      }
-    )
-    if (res && res.data) {
-      console.log("new token",res);
-      localStorage.setItem('accessToken', res.data.access_token);
-    }
-  }
-
-  const getContacts = () => {
-    let tags: any = []
-    let token = localStorage.getItem('accessToken');
-    if (token) {
-      axios.get(`https://api-im.chatdaddy.tech/contacts?count=${count}&tags=${tags}`,
-        {
-          headers: {
-            "Content-type": "Application/json",
-            "Authorization": `Bearer ${token}`
-          }
-        }
-      ).then(async response => {
-        console.log(response.data.contacts);
-        setContacts(response.data.contacts);
-      }).catch((error: AxiosError) => {
-        console.log(error.response);
-        updateToken().then(getContacts);
-      });
-    } else {
-      updateToken().then(getContacts);
-    }
-    count += 10
-  }
-
-  const handleScroll = (e:any) => {
-    if (window.innerHeight + e.target.documentElement.scrollTop + 1 >= e.target.documentElement.scrollHeight) {
-      getContacts()
-    }
-  }
-
   const handleSelectAll = (e:any) => {
     setIsCheckAll(!isCheckAll);
-    setIsCheck(contacts.map(contact => contact.id));
+    setIsCheck(props.contacts.map((contact:any) => contact.id));
     if (isCheckAll) {
       setIsCheck([]);
     }
@@ -86,12 +41,7 @@ function Contacts(props:any) {
     console.log(isCheck);
   };
 
-  useEffect(() => {
-    getContacts()
-    window.addEventListener('scroll', handleScroll)
-  }, [])
-
-  return (
+    return (
     <div style={{width: '100%'}}>
       {/* <CssBaseline /> */}
       {/* <Container maxWidth="sm"> */}
@@ -100,7 +50,7 @@ function Contacts(props:any) {
         checked={isCheckAll}  name="selectAll"
         id="selectAll" /> Select All
           {
-            contacts.map(contact => {
+            props.contacts.map((contact:any) => {
               return <ContactCard name={contact.name} handleClick={handleClick} phoneNumber={contact.phoneNumber} id={contact.id} isCheck={isCheck} tags={contact.tags} />
             })
           }
